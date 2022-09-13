95ce4d08f3aaa6eafca8b125949ecf3ee0020720
Cal Rueb <cal@stytch.com>
2022-09-13 16:02:02 -0700

    Landing page layout complete

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,60 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import { createTheme, ThemeProvider } from "@mui/material";
+import type { AppProps } from "next/app";
+import { OTPMethods } from "@stytch/vanilla-js";
+
+const theme = createTheme({
+  typography: {
+    allVariants: {
+      fontFamily: "'IBM Plex Sans', sans-serif;",
+      textTransform: "none",
+      color: "#0D0000",
+    },
+    h1: {
+      fontSize: "40px",
+      lineHeight: "60px",
+      fontWeight: 600,
+    },
+    h2: {
+      fontSize: "30px",
+      lineHeight: "40px",
+      fontWeight: 600,
+    },
+    h3: {
+      fontSize: "24px",
+      lineHeight: "30px",
+      fontWeight: 500,
+      color: "#000000",
+    },
+    h4: {
+      fontSize: "20px",
+      lineHeight: "30px",
+      fontWeight: 400,
+    },
+    caption: {
+      fontSize: 16,
+      lineHeight: "20px",
+    },
+    body1: {
+      fontSize: "18px",
+      lineHeight: "25px",
+      fontWeight: 400,
+      color: "#19303D",
+    },
+    body2: {
+      fontSize: "14px",
+      lineHeight: "20px",
+      fontWeight: 400,
+    },
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
